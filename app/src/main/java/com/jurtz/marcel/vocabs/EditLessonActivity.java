b69c90a0edb2a26afc9ff54772ff4bfa510bf9d0
MarcelJurtz <jurtzmarcel@gmail.com>
2017-10-12 16:41:40 +0200

    Add initial activities and basic contents

---
+++ app/src/main/java/com/jurtz/marcel/vocabs/EditLessonActivity.java
@@ -0,0 +1,13 @@
+package com.jurtz.marcel.vocabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditLessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_lesson);
+    }
+}
